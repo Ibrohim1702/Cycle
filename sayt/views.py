bc13df729e5ebb5f3aad43ac960043b27bd23c8a
Ibrohim1702 <88259165+Ibrohim1702@users.noreply.github.com>
2023-07-06 15:37:36 +0500

    Views
    Views urls,

--- sayt/views.py
+++ sayt/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
 
+from sayt.models import Contact
+
+
 # Create your views here.
+
+
+def index(requests):
+    ctx = {
+
+    }
+    return render(requests, "index.html", ctx)
+
+
+def about(requests):
+    ctx = {
+
+    }
+    return render(requests, "about.html", ctx)
+
+
+def cycle(requests):
+    ctx = {
+
+    }
+    return render(requests, "cycle.html", ctx)
+
+
+def news(requests):
+    ctx = {
+
+    }
+    return render(requests, "news.html", ctx)
+
+
+
+def contact(requests):
+    ctx = {}
+    if requests.POST:
+        name = requests.POST.get('name')
+        message = requests.POST.get('message')
+        phone = requests.POST.get('phone')
+        email = requests.POST.get('email')
+        Contact.objects.create(
+            name=name, message=message, phone=phone, email=email
+        )
+
+        ctx = {
+            "contact": contact,
+
+        }
+    return render(requests, "contact.html", ctx)
